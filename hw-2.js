376b3d01d3a888f89439545a4945f59d36fe719b
ssashoq <gheroes.95@mail.ru>
2023-02-21 20:08:44 +0900

    Changed file hw-2.js

--- hw-2.js
+++ hw-2.js
@@ -19,6 +19,24 @@ alert(b - c);
 alert(b * c);
 alert(b / c);
 
-let result = (c**5);
+let result = (c ** 5);
 
 alert(result);
+
+let d = 9;
+let e = 2;
+
+alert(d % e);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
