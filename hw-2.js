b8dccc9b9025d8553d371bec640a613e931dc2d8
ssashoq <gheroes.95@mail.ru>
2023-02-21 19:55:09 +0900

    Changed file hw-2.js

---
+++ hw-2.js
@@ -0,0 +1,24 @@
+let a = 20;
+
+alert(a);
+
+let iphone = 2007;
+
+alert(iphone);
+
+let JS = 'JavaScript';
+
+alert(JS);
+
+let b = 10;
+
+let c = 2;
+
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+let result = (c**5);
+
+alert(result);
