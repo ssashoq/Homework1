e4194913306f4ba72b1e7da6cb5401b937f65dcd
ssashoq <gheroes.95@mail.ru>
2023-02-28 19:06:50 +0900

    changed after comments hw-3.js

--- hw-3.js
+++ hw-3.js
@@ -1,6 +1,6 @@
 let password = 'пароль';
 let user = prompt('Введите пароль');
-user === password ? alert('Пароль введен верно') : alert ('Пароль введен неправильно');
+alert(user === password ? 'Пароль введен верно' : 'Пароль введен неправильно');
 
 let c = 2;c>0 && c<10 ? alert('Верно') : alert('Неверно');
 
@@ -15,32 +15,21 @@ alert(a+b);
 let monthNumber = Number(prompt('Введите номер месяца'));
 switch(monthNumber) {
     case 1:
-        console.log('Зима');
-        break;
     case 2:
         console.log('Зима');
         break;
-    case 3:
-        console.log('Весна');
-        break;   
+    case 3:   
     case 4:
-        console.log('Весна');
-        break;
     case 5:
         console.log('Весна');
         break;
     case 6:
+    case 7:
+    case 8:
         console.log('Лето');
         break;
-    case 7:
-        console.log('Весна');
-        break;
-    case 9:
-        console.log('Осень');
-        break;   
+    case 9:  
     case 10:
-        console.log('Осень');
-        break;
     case 11:
         console.log('Осень');
         break;
